d7bc9f16463be72213ee678c3ed030e06891ee0c
michaelgappa22 <m.gappa2293@gmail.com>
2023-06-13 15:00:25 -0400

    Add Custom Workflows for Case Assignment. Need to add Assignment Step in Workflows.

--- RoundRobin/RoundRobin.cs
+++ RoundRobin/RoundRobin.cs
@@ -81,4 +81,56 @@ namespace RoundRobin
         }
     }
 
+    public class GetTeamHoursOfOperations : CodeActivity
+    {
+        [Input("Team")]
+        [ReferenceTarget("team")]
+        public InArgument<EntityReference> Team { get; set; }
+
+        [Output("Closed Date and Time")]
+        public OutArgument<DateTime> ClosedDateTime { get; set; }
+
+        [Output("Open Date and Time")]
+        public OutArgument<DateTime> OpenDateTime { get; set; }
+
+        [Output("Count of Users Login")]
+        public OutArgument<int> CountOfUsersLogin { get; set; }
+
+        [Output("Total Members of Team")]
+        public OutArgument<int> OpenTotalMembersOfTeam { get; set; }
+
+        [Output("Round Robin Threshold")]
+        public OutArgument<DateTime> RoundRobinThreshold { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IOrganizationService service = context.GetExtension<IOrganizationServiceFactory>().CreateOrganizationService(null);
+            EntityReference teamRef = Team.Get(context);
+            Entity teamRecord = service.Retrieve(teamRef.LogicalName, teamRef.Id, new ColumnSet("ramcosub_opendatetime","ramcosub_closedatetime", "ramcosub_countofloginusers", "ramcosub_totalteammembers", "ramcosub_roundrobinthreshold"));
+
+            DateTime closedDateTime = teamRecord.GetAttributeValue<DateTime>("ramcosub_closedatetime");
+            DateTime openDateTime = teamRecord.GetAttributeValue<DateTime>("ramcosub_opendatetime");
+            DateTime getRoundRobinThreshold = teamRecord.GetAttributeValue<DateTime>("ramcosub_roundrobinthreshold");
+            int countOfUsersLogin = teamRecord.GetAttributeValue<int>("ramcosub_countofloginusers");
+            int openTotalMembersOfTeam = teamRecord.GetAttributeValue<int>("ramcosub_totalteammembers");
+            
+
+            ClosedDateTime.Set(context, closedDateTime);
+            OpenDateTime.Set(context, openDateTime);
+            CountOfUsersLogin.Set(context, countOfUsersLogin);
+            OpenTotalMembersOfTeam.Set(context, openTotalMembersOfTeam);
+            RoundRobinThreshold.Set(context, getRoundRobinThreshold);
+        }
+    }
+
+    public class RoundRobinAssignment
+    {
+        [Input("Team")]
+        [ReferenceTarget("team")]
+        public InArgument<EntityReference> Team { get; set; }
+        [Input("Case")]
+        [ReferenceTarget("incident")]
+        public InArgument<EntityReference> Case { get; set; }
+    }
+
 }
