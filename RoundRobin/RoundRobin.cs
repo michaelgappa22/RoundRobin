5bf6ec72bd1bb7e4602bea56cc1971b03d55211b
michaelgappa22 <m.gappa2293@gmail.com>
2023-06-26 22:35:13 -0400

    updated fixed code.

--- RoundRobin/RoundRobin.cs
+++ RoundRobin/RoundRobin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Activities;
+using System.Linq;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Workflow;
@@ -107,7 +108,6 @@ namespace RoundRobin
             IOrganizationService service = context.GetExtension<IOrganizationServiceFactory>().CreateOrganizationService(null);
             EntityReference teamRef = Team.Get(context);
             Entity teamRecord = service.Retrieve(teamRef.LogicalName, teamRef.Id, new ColumnSet("ramcosub_opendatetime","ramcosub_closedatetime", "ramcosub_countofloginusers", "ramcosub_totalteammembers", "ramcosub_roundrobinthreshold"));
-
             DateTime closedDateTime = teamRecord.GetAttributeValue<DateTime>("ramcosub_closedatetime");
             DateTime openDateTime = teamRecord.GetAttributeValue<DateTime>("ramcosub_opendatetime");
             DateTime getRoundRobinThreshold = teamRecord.GetAttributeValue<DateTime>("ramcosub_roundrobinthreshold");
@@ -123,14 +123,130 @@ namespace RoundRobin
         }
     }
 
-    public class RoundRobinAssignment
+    public class RoundRobinAssignment : CodeActivity
     {
         [Input("Team")]
         [ReferenceTarget("team")]
-        public InArgument<EntityReference> Team { get; set; }
+        public InArgument<EntityReference> TeamRecord { get; set; }
+
+        [Input("System View")]
+        [ReferenceTarget("savedquery")]
+        public InArgument<EntityReference> SystemView { get; set; }
+
         [Input("Case")]
-        [ReferenceTarget("incident")]
-        public InArgument<EntityReference> Case { get; set; }
+        [ReferenceTarget("new_case")]
+        public InArgument<EntityReference> CaseRecord { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {
+                IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+                IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+                // get Team Record, System View, and Created Case Record
+                EntityReference teamRef = TeamRecord.Get(context);
+                EntityReference savedView = SystemView.Get(context);
+                EntityReference caseRecord = CaseRecord.Get(context);
+
+                EntityReference teamQueue = GetTeamQueue(teamRef, service);
+                EntityCollection queueItems = GetQueueItems(savedView, service);
+                EntityCollection teamMembers = GetTeamMembers(teamRef, service);
+                Entity teamRecord = GetLastUserName(teamRef, service);
+                // retrieves EntityReference object
+                EntityReference lastAssignedUser = teamRecord.GetAttributeValue<EntityReference>("ramcosub_lastuserassigned");
+                AssignQueueItemToUser(queueItems, teamMembers, lastAssignedUser, service);
+                CreateCaseQueueItem(caseRecord, teamQueue, lastAssignedUser, service);
+            }
+
+            catch (Exception ex)
+            {
+                // Handle or log the exception here
+                throw new InvalidPluginExecutionException("An error occurred: " + ex.Message);
+            }
+        }
+
+        private static EntityCollection GetQueueItems(EntityReference savedView, IOrganizationService service)
+        {
+            ColumnSet columns = new ColumnSet("name", "fetchxml");
+            Entity systemView = service.Retrieve("savedquery", savedView.Id, columns);
+            string fetchXml = systemView.GetAttributeValue<string>("fetchxml");
+            EntityCollection results = service.RetrieveMultiple(new FetchExpression(fetchXml));
+            return results;
+        }
+
+        private static EntityCollection GetTeamMembers(EntityReference teamRef, IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression("ramcosub_userlogin");
+            query.Criteria.AddCondition("ramcosub_team", ConditionOperator.Equal, teamRef.Id);
+            query.Criteria.AddCondition("createdon", ConditionOperator.OnOrAfter, DateTime.Today);
+            query.Criteria.AddCondition("statecode", ConditionOperator.Equal, 0);
+            query.ColumnSet.AddColumn("ramcosub_user");
+            EntityCollection teamMembers = service.RetrieveMultiple(query);
+            return teamMembers;
+        }
+
+        private static void CreateCaseQueueItem(EntityReference caseRecord, EntityReference teamQueue, EntityReference lastAssignedUser, IOrganizationService service)
+        {
+            Entity queueItem = new Entity("queueitem");
+            queueItem["objecttypecode"] = new OptionSetValue(112);  // Set the object type code of the entity being queued
+            queueItem["objectid"] = new EntityReference(caseRecord.LogicalName, caseRecord.Id);  // Set the entity reference of the record being queued
+            queueItem["queueid"] = new EntityReference("queue", teamQueue.Id);  // Set the entity reference of the target queue
+            queueItem["workerid"] = lastAssignedUser;
+            service.Create(queueItem);
+        }
+
+        private static Entity GetLastUserName(EntityReference teamRef, IOrganizationService service)
+        {
+            Entity teamRecord = service.Retrieve(teamRef.LogicalName, teamRef.Id, new ColumnSet("ramcosub_lastuserassigned"));
+            return teamRecord;
+        }
+
+        private static EntityReference GetTeamQueue(EntityReference teamRef, IOrganizationService service)
+        {
+            Entity team = service.Retrieve(teamRef.LogicalName, teamRef.Id, new ColumnSet("queueid"));
+            EntityReference teamQueue = team.GetAttributeValue<EntityReference>("queueid");
+            return teamQueue;
+        }
+        private static void AssignQueueItemToUser(EntityCollection queueItems, EntityCollection teamMembers, EntityReference lastAssignedUser, IOrganizationService service)
+        {
+            int teamMemberIndex = 0;
+            if (lastAssignedUser == null)
+            {
+
+
+                foreach (Entity queueItem in queueItems.Entities)
+                {
+                    if (teamMemberIndex >= teamMembers.Entities.Count)
+                        teamMemberIndex = 0;
+                    EntityReference userRecordRef = new EntityReference("systemuser", teamMembers.Entities[teamMemberIndex].Id);
+                    queueItem["workerid"] = userRecordRef;
+                    service.Update(queueItem);
+
+                    teamMemberIndex++;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < teamMembers.Entities.Count; i++)
+                {
+                    if (teamMembers.Entities[i].GetAttributeValue<EntityReference>("ramcosub_user").Name == lastAssignedUser.Name)
+                    {
+                        teamMemberIndex = i;
+                    }
+                }
+                foreach (Entity queueItem in queueItems.Entities)
+                {
+                    if (teamMemberIndex >= teamMembers.Entities.Count)
+                        teamMemberIndex = 0;
+
+                    EntityReference userRecordRef = new EntityReference("systemuser", teamMembers.Entities[teamMemberIndex].GetAttributeValue<EntityReference>("ramcosub_user").Id);
+                    queueItem["workerid"] = userRecordRef;
+                    service.Update(queueItem);
+
+                    teamMemberIndex++;
+                }
+            }
+        }
     }
 
 }
